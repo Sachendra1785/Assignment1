97ec476d49406ebfae9776df3ab52ba9927cdd54
Sachendra <Sachendra@HP-1500>
2024-08-12 13:54:20 +0530

    New class Added

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Class1
+    {
+        public void printData() {
+            Console.WriteLine();
+                }
+    }
+}
